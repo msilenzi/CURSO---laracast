67e80535fe9f9e6160a1f678d7f386be50ad37b9
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-07-23 18:44:49 -0300

    47. add Composer and composer-autoload

--- section03_notes_project/public/index.php
+++ section03_notes_project/public/index.php
@@ -3,16 +3,13 @@
 use core\Session;
 use core\ValidationException;
 
-session_start();
-
 const BASE_PATH = __DIR__ . "/../";
+
+require BASE_PATH . '/vendor/autoload.php';
 
 require BASE_PATH . 'core/functions.php';
 
-spl_autoload_register(function ($class) {
-  $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-  req_base("{$class}.php");
-});
+session_start();
 
 req_data('bootstrap.php');
 
