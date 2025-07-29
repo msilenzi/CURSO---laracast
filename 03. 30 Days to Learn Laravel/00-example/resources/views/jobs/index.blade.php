1c3134f5260e8f8e623127013d872ec183574134
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-07-29 20:05:29 -0300

    form submit & csrf

--- 03. 30 Days to Learn Laravel/00-example/resources/views/jobs/index.blade.php
+++ 03. 30 Days to Learn Laravel/00-example/resources/views/jobs/index.blade.php
@@ -4,11 +4,15 @@
 
     <div class="grid grid-cols-[repeat(auto-fill,minmax(400px,1fr))] gap-4">
         @foreach($jobs as $job)
-            <a href="/jobs/{{$job->id}}" class="block px-4 py-6 bg-gray-50 rounded-md shadow-md border border-gray-100 hover:shadow-lg hover:text-underline transition-all duration-600 ease-out">
+            <a href="/jobs/{{$job->id}}" class="block px-4 py-6 bg-gray-50 rounded-md shadow-md border border-gray-100 hover:shadow-xl hover:text-underline hover:z-10 hover:scale-102 transition-all duration-400 ease-out">
                 <span class="text-neutral-500 uppercase text-xs">{{$job->employer->name}}</span>
                 <h2 class="font-bold text-2xl block">{{$job->title}}</h2>
                 <p class="font-semibold text-indigo-500 text-2xl">{{$job->salary}}</p>
             </a>
         @endforeach
+    </div>
+
+    <div>
+        {{$jobs->links()}}
     </div>
 </x-layout>
--- 03. 30 Days to Learn Laravel/00-example/routes/web.php
+++ 03. 30 Days to Learn Laravel/00-example/routes/web.php
@@ -4,25 +4,41 @@ use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', [
-        'title' => 'Home'
+    return view('home', ['title' => 'Home']);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact',]);
+});
+
+/*
+ * Jobs ========================================================================
+ */
+
+Route::get('/jobs', function() {
+    return view('jobs.index', [
+        'title' => 'Jobs',
+        'jobs' => Job::with('employer')->latest()->paginate(10)
     ]);
 });
 
-Route::get('/jobs', function() {
-    return view('jobs', [
-        'title' => 'Jobs',
-        'jobs' => Job::with('employer')->get()
-    ]);
+Route::get('/jobs/create', function() {
+    return view('jobs.create', ['title' => 'Create Job']);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    return view('job', [
+    return view('jobs.show', [
         'title' => 'Job',
         'job' => Job::find($id)
     ]);
 });
 
-Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact',]);
+Route::post('/jobs', function() {
+    $newJob = Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect("/jobs/{$newJob['id']}");
 });
