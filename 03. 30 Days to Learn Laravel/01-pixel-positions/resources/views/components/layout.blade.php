45baa1484bcca91c5d2dfeb75129d84b32e41ff6
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-08-11 18:35:56 -0300

    create basic layout

---
+++ 03. 30 Days to Learn Laravel/01-pixel-positions/resources/views/components/layout.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel Positions</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-black text-white">
+    <header class="flex justify-between items-center px-10 py-4 border-b border-white/10">
+        <a href="#">
+            <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="Site Logo" />
+        </a>
+
+        <nav>
+            <ul class="flex justify-center items-center gap-10 font-bold">
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Salaries</a></li>
+                <li><a href="#">Companies</a></li>
+            </ul>
+        </nav>
+
+        <a href="#">Post a Job</a>
+    </header>
+
+    <main class="mt-10 max-w-[986px] container mx-auto">
+        {{ $slot }}
+    </main>
+</body>
+</html>
