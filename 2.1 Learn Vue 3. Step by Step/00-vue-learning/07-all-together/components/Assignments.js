fee4868d7540c9e81acb022911b06ab4aefa3f83
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-09-13 12:01:19 -0300

    add TagSelector

--- 2.1 Learn Vue 3. Step by Step/00-vue-learning/07-all-together/components/Assignments.js
+++ 2.1 Learn Vue 3. Step by Step/00-vue-learning/07-all-together/components/Assignments.js
@@ -1,5 +1,6 @@
 import AssignmentList from "./AssignmentList.js";
 import AssignmentCreate from "./AssignmentCreate.js";
+import TagSelector from "./TagSelector.js";
 
 export default {
   template: `
@@ -7,6 +8,22 @@ export default {
       <h1 class="text-2xl font-bold">Assignments</h1>
       
       <assignment-create @create="addAssignment" />
+
+      <div class="flex flex-wrap gap-2">
+        <tag-selector
+          tag="all"
+          :currentTag="currentTag"
+          @selected="setCurrentTag"
+        />
+
+        <tag-selector
+          v-for="tag in tags"
+          :key="tag"
+          :tag="tag"
+          :currentTag="currentTag"
+          @selected="setCurrentTag"
+        />
+      </div>
 
       <assignment-list title="Pending" :assignments="pending" />
       <assignment-list title="Completed" :assignments="completed" />
@@ -20,22 +37,36 @@ export default {
         { id: 2, name: "Read chapter 4", completed: false, tag: "literature" },
         { id: 3, name: "Read chapter 5", completed: false, tag: "literature" },
       ],
+      currentTag: "all",
     };
   },
 
   computed: {
     completed() {
-      return this.assignments.filter((a) => a.completed);
+      return this.assignments.filter(
+        (a) =>
+          a.completed &&
+          (this.currentTag === "all" || a.tag === this.currentTag)
+      );
     },
 
     pending() {
-      return this.assignments.filter((a) => !a.completed);
+      return this.assignments.filter(
+        (a) =>
+          !a.completed &&
+          (this.currentTag === "all" || a.tag === this.currentTag)
+      );
+    },
+
+    tags() {
+      return new Set(this.assignments.map(({ tag }) => tag));
     },
   },
 
   components: {
     AssignmentList,
-    AssignmentCreate
+    AssignmentCreate,
+    TagSelector,
   },
 
   methods: {
@@ -46,6 +77,10 @@ export default {
         completed: false,
         id: this.assignments.at(-1).id + 1,
       });
+    },
+
+    setCurrentTag(newCurrentTag) {
+      this.currentTag = newCurrentTag;
     },
   },
 };
