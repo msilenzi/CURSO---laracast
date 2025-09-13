8b282874efd6fd391366772e75a43250dd78d372
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-09-13 11:30:46 -0300

    show assinments length

--- 2.1 Learn Vue 3. Step by Step/00-vue-learning/07-all-together/components/AssignmentList.js
+++ 2.1 Learn Vue 3. Step by Step/00-vue-learning/07-all-together/components/AssignmentList.js
@@ -3,7 +3,10 @@ import AssignmentListItem from "./AssignmentListItem.js"
 export default {
   template: `
     <section>
-      <h2 class="mb-2 text-xl font-bold">{{ title }}</h2>
+      <h2 class="mb-2 text-xl font-bold">
+        {{ title }}
+        <span class="text-slate-500 text-sm"> ({{ assignments.length }})</span>
+      </h2>
       <ul v-if="assignments.length" class="divide-y divide-slate-600 rounded-lg overflow-hidden">
         <assignment-list-item 
           v-for="assignment in assignments"
